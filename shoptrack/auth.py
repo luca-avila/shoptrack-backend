57acbc3ad45c38eeadf45c066e7fd1834deb955f
luca-avila <avilaluca61@gmail.com>
2025-08-21 15:04:42 -0300

    Modify register function to only receive POST method

--- shoptrack/auth.py
+++ shoptrack/auth.py
@@ -1,7 +1,8 @@
 import functools
+import sqlite3
 
 from flask import (
-    Blueprint, g, redirect, request, session, url_for, jsonify
+    Blueprint, g, redirect, request, session, url_for, jsonify, current_app
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,29 +10,30 @@ from shoptrack.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        data = request.get_json()
-        db = get_db()
-        error = None
+    data = request.get_json()
+    db = get_db()
+    error = None
 
-        if not data['username']:
-            error = 'Username is required.'
-        elif not data['password']:
-            error = 'Password is required.'
+    if not data['username']:
+        error = 'Username is required.'
+    elif not data['password']:
+        error = 'Password is required.'
 
-        if error is not None:
-            return jsonify({'error': error}), 400
-        
-        try:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (data['username'], generate_password_hash(data['password']))
-            )
-            db.commit()
-        except db.IntegrityError:
-            error = f"User {data['username']} is already registered."
-            return jsonify({'error': error}), 400
-        
-        return jsonify({'message': 'User registered successfully.'}), 201
+    if error is not None:
+        return jsonify({'error': error}), 400
+    
+    try:
+        db.execute(
+            'INSERT INTO user (username, password) VALUES (?, ?)',
+            (data['username'], generate_password_hash(data['password']))
+        )
+        db.commit()
+    except sqlite3.IntegrityError:
+        error = f"User {data['username']} is already registered."
+        return jsonify({'error': error}), 400
+    
+    return jsonify({'message': 'User registered successfully.'}), 201
+    
+    
