6f87b823e4468bb7f1051fb21c0cc976b3f27086
luca-avila <avilaluca61@gmail.com>
2025-08-25 12:43:40 -0300

    Change schema.sql queries to match postgresql syntax

--- shoptrack/schema.sql
+++ shoptrack/schema.sql
@@ -4,32 +4,32 @@ DROP TABLE IF EXISTS history;
 DROP TABLE IF EXISTS sessions;
 
 CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE product (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     stock INTEGER NOT NULL CHECK (stock >= 0),
     price REAL NOT NULL CHECK (price > 0),
     description TEXT,
     owner_id INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (owner_id) REFERENCES user (id)
 );
 
 CREATE TABLE history (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     product_id INTEGER NOT NULL,
     product_name TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     price REAL NOT NULL CHECK (price > 0),
     quantity INTEGER NOT NULL CHECK (quantity > 0),
     action TEXT NOT NULL CHECK (action IN ('buy', 'sell')),
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (product_id) REFERENCES product (id),
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
@@ -37,7 +37,7 @@ CREATE TABLE history (
 CREATE TABLE sessions (
     id TEXT PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
     expires TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
