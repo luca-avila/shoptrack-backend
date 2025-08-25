fcce564e5ff55f64b384337e2df47c591ddd90ea
luca-avila <avilaluca61@gmail.com>
2025-08-25 12:56:44 -0300

    Change schema.sql to match sqlite3 syntax, to add and test SQLAlchemy

--- shoptrack/schema.sql
+++ shoptrack/schema.sql
@@ -4,32 +4,32 @@ DROP TABLE IF EXISTS history;
 DROP TABLE IF EXISTS sessions;
 
 CREATE TABLE user (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT NOW()
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE product (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     stock INTEGER NOT NULL CHECK (stock >= 0),
     price REAL NOT NULL CHECK (price > 0),
     description TEXT,
     owner_id INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (owner_id) REFERENCES user (id)
 );
 
 CREATE TABLE history (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     product_id INTEGER NOT NULL,
     product_name TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     price REAL NOT NULL CHECK (price > 0),
     quantity INTEGER NOT NULL CHECK (quantity > 0),
     action TEXT NOT NULL CHECK (action IN ('buy', 'sell')),
-    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (product_id) REFERENCES product (id),
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
@@ -37,7 +37,7 @@ CREATE TABLE history (
 CREATE TABLE sessions (
     id TEXT PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expires TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
